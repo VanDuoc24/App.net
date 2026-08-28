5c051d91a9d90c6ad3e028bf357bfad08db1c377
VanDuoc24 <hoangvan24112006@gmail.com>
2026-08-28 20:23:42 +0700

    Add files via upload

---
+++ Models/hvdFroduct.cs
@@ -0,0 +1,12 @@
+﻿namespace hvduocLesson3.Models
+{
+    public class hvdFroduct
+    {
+        public string hvdFroductId { get; set; }
+        public string hvdFroductName { get; set; }  
+        public int hvdYearRelease { get; set; }
+        public decimal  hvdPrive { get; set; }
+
+
+    }
+}
